d35adc284e14aec1aeffb8efab27389fcabb56cb
Rusiek <79122265+Rusiek@users.noreply.github.com>
2021-05-09 00:38:27 +0200

    Update Testerka.sh

--- Testerka.sh
+++ Testerka.sh
@@ -1,3 +1,4 @@
+wstart=`date +%s%N`
 for t in $2/*.in; do
 start=`date +%s%N`
 echo $q
@@ -6,3 +7,5 @@ diff -bsq $o <(./$1 < $t) || break
 end=`date +%s%N`
 printf "%.3f%s\n" $(((end-start)/(10**6)))e-3 s
 done
+wend=`date +%s%N`
+printf "%s%.3f%s\n" Wszystkie testy zaliczone w $(((wend-wstart)/(10**6)))e-3 sekund!
