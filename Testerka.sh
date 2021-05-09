e2ad3b141c76077f5c905e4f297fe9955dc95764
Rusiek <79122265+Rusiek@users.noreply.github.com>
2021-05-09 14:32:08 +0200

    Update Testerka.sh

--- Testerka.sh
+++ Testerka.sh
@@ -3,5 +3,5 @@ start=`date +%s%N`
 test=${file%.in}.out
 diff -bsq $test <(./$1 < $file) || break
 end=`date +%s%N`
-printf "%.3f%s\n" $(((end-start)/(10**6)))e-3 s
+printf "\n%.3f%s\n" $(((end-start)/(10**6)))e-3 s
 done
