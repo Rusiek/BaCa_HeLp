74ee09904da01be416ee8b47dc8680161dce7ee7
Rusiek <79122265+Rusiek@users.noreply.github.com>
2021-05-09 14:03:26 +0200

    Update Testerka.sh

--- Testerka.sh
+++ Testerka.sh
@@ -1,8 +1,8 @@
-for t in $2/*.in; do
+for file in $2/*.in; do
 start=`date +%s%N`
 echo $q
-o=${t%.in}.out
-diff -bsq $o <(./$1 < $t) || break
+test=${file%.in}.out
+diff -bsq $test <(./$1 < $file) || break
 end=`date +%s%N`
 printf "%.3f%s\n" $(((end-start)/(10**6)))e-3 s
 done
